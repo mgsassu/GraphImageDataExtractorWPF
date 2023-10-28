9c7578a156b27ba1a0b80efe31f1d92b6c44a9ad
mgsassu <44040709+mgsassu@users.noreply.github.com>
2023-10-27 18:29:21 -0600

    Can get colors of pixels from within the image. Tested JPEG and PNG, both of which appear to work.

--- GraphImageDataExtractorWPF/MainWindow.xaml.cs
+++ GraphImageDataExtractorWPF/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,20 +23,141 @@ namespace GraphImageDataExtractorWPF
    /// <summary>
    /// Interaction logic for MainWindow.xaml
    /// </summary>
-   public partial class MainWindow : Window
+   public partial class MainWindow : Window , INotifyPropertyChanged
    {
-      private BitmapImage bitmapImage = new BitmapImage();
-      
+      #region Private Properties
+      private BitmapImage bitmapImage;
+      private int bytesPerPixel = 4;
+      private byte[] pixels = new byte[0];
+      private bool setColor = false;
+      private int rMove = 0;
+      private int gMove = 0;
+      private int bMove = 0;
+      private int rSaved = 0;
+      private int gSaved = 0;
+      private int bSaved = 0;
+      #endregion
+
+      #region Constructor
       public MainWindow()
       {
          InitializeComponent();
+         DataContext = this;
+      }
+      #endregion
+
+      #region Public Properties
+      /// <summary>
+      /// Current R value
+      /// </summary>
+      public int RMove
+      {
+         get
+         {
+            return rMove;
+         }
+         set
+         {
+            rMove = value;
+            OnPropertyChanged(nameof(RMove));
+         }
       }
 
+      /// <summary>
+      /// Current G value
+      /// </summary>
+      public int GMove
+      {
+         get
+         {
+            return gMove;
+         }
+         set
+         {
+            gMove = value;
+            OnPropertyChanged(nameof(GMove));
+         }
+      }
 
+      /// <summary>
+      /// Current B value
+      /// </summary>
+      public int BMove
+      {
+         get
+         {
+            return bMove;
+         }
+         set
+         {
+            bMove = value;
+            OnPropertyChanged(nameof(BMove));
+         }
+      }
 
-      #region Event Handlers
+      /// <summary>
+      /// Current R value
+      /// </summary>
+      public int RSaved
+      {
+         get
+         {
+            return rSaved;
+         }
+         set
+         {
+            rSaved = value;
+            OnPropertyChanged(nameof(RSaved));
+         }
+      }
+
+      /// <summary>
+      /// Current G value
+      /// </summary>
+      public int GSaved
+      {
+         get
+         {
+            return gSaved;
+         }
+         set
+         {
+            gSaved = value;
+            OnPropertyChanged(nameof(GSaved));
+         }
+      }
+
+      /// <summary>
+      /// Current B value
+      /// </summary>
+      public int BSaved
+      {
+         get
+         {
+            return bSaved;
+         }
+         set
+         {
+            bSaved = value;
+            OnPropertyChanged(nameof(BSaved));
+         }
+      }
+      #endregion
+
+      #region Event Handlers and Functions
+      public event PropertyChangedEventHandler? PropertyChanged;
+
+      protected void OnPropertyChanged([CallerMemberName] string name = "")
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+      }
+
       private void btnLoadImage_Click(object sender, RoutedEventArgs e)
       {
+         // New object
+         bitmapImage = new BitmapImage();
+
+         // Open file
          OpenFileDialog ofd = new OpenFileDialog();
          ofd.Multiselect = false;
          ofd.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
@@ -44,8 +168,59 @@ namespace GraphImageDataExtractorWPF
             bitmapImage.EndInit();
          }
 
+         // Get pixel array separately in memory
+         GetPixels(bitmapImage, ref pixels);
+
+         // Set as source
          imageControl.Source = bitmapImage;
          imageControl.UpdateLayout();
+      }
+
+      private void btnSetColor_Click(object sender, RoutedEventArgs e)
+      {
+         setColor = true;
+      }
+
+      private void imageControl_MouseMove(object sender, MouseEventArgs e)
+      {
+         Point p = e.GetPosition(imageControl);
+         int i = ((int)p.X + (int)p.Y * (int)bitmapImage.Width) * bytesPerPixel;
+         
+         BMove = pixels[i];
+         GMove = pixels[i+1];
+         RMove = pixels[i+2];
+      }
+
+      private void imageControl_MouseUp(object sender, MouseButtonEventArgs e)
+      {
+         // Set the selected colors.
+         if (setColor)
+         {
+            Point p = e.GetPosition(imageControl);
+            int i = ((int)p.X + (int)p.Y * (int)bitmapImage.Width) * bytesPerPixel;
+
+            BSaved = pixels[i];
+            GSaved = pixels[i + 1];
+            RSaved = pixels[i + 2];
+         }
+
+         // Set bool back to false so chosen colors aren't overwritten
+         setColor= false;
+      }
+
+      #endregion
+
+      #region Private Methods
+      public void GetPixels(BitmapSource source, ref byte[] pc)
+      {
+         if (source.Format != PixelFormats.Bgra32)
+            source = new FormatConvertedBitmap(source, PixelFormats.Bgra32, null, 0);
+         
+         int width = source.PixelWidth;
+         int height = source.PixelHeight;
+
+         pc = new byte[width * height * bytesPerPixel];
+         source.CopyPixels(pc, width * bytesPerPixel, 0);
       }
 
       #endregion
